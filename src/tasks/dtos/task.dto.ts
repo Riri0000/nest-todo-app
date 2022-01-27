fae0660decca7f739eb8d325bca5f8eb77968b37
Riri0000 <ririko010102@gmail.com>
2022-01-27 21:17:55 +0900

    Add: TaskService

---
+++ src/tasks/dtos/task.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class TaskDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  note: string;
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectRepository(Task) private repo: Repository<Task>) {}
+
+  // create entity instance
+  create(name: string, note: string, status: string) {
+    const task = this.repo.create({ name, note, status });
+
+    // save to DB
+    return this.repo.save(task);
+  }
+
+  findOne(id: number) {
+    if (!id) {
+      return null;
+    }
+    return this.repo.findOne(id);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  async update(id: number, status: string): Promise<Task> {
+    const task = await this.findOne(id);
+    if (!task) {
+      throw new NotFoundException('Task is not found');
+    }
+    task.status = status;
+    return this.repo.save(task);
+  }
+
+  async delete(id: number): Promise<Task> {
+    const task = await this.findOne(id);
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    return this.repo.remove(task);
+  }
+}
